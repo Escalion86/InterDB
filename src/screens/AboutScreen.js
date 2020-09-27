db58d56895ca27e1d817be81d246028ae7bb79d8
Aleksei Belinskiy <escalion86@gmail.com>
2020-09-27 21:56:05 +0700

    Stylized AboutScreen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,9 +1,12 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Linking, Image } from "react-native"
 import { HeaderButtons, Item } from "react-navigation-header-buttons"
 import { AppHeaderIcon } from "../components/AppHeaderIcon"
 import burgerButton from "../components/burgerButton"
 import { useTheme } from "@react-navigation/native"
+import { colors } from "react-native-elements"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import { FontAwesome5 } from "@expo/vector-icons"
 
 const AboutScreen = ({ navigation, route }) => {
 	const { colors } = useTheme()
@@ -30,14 +33,103 @@ const AboutScreen = ({ navigation, route }) => {
 		// ),
 	})
 
+	const ContactIcon = ({
+		iconName = "",
+		backgroundColor = colors.background,
+		url = null,
+	}) => {
+		const size = 30
+		return (
+			<TouchableOpacity
+				onPress={() => {
+					url ? Linking.openURL(url) : null
+				}}
+				style={{
+					...styles.contact,
+					width: size + Math.floor(size / 2),
+					height: size + Math.floor(size / 2),
+					padding: Math.floor(size / 16),
+					backgroundColor: backgroundColor,
+				}}
+			>
+				<FontAwesome5 name={iconName} size={size} color={"white"} />
+			</TouchableOpacity>
+		)
+	}
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.content}>
-				<Text style={{ fontSize: 18, color: colors.text }}>
-					Цель приложения - упрощение взаимодействия с клиентами при продаже
-					своих услуг. На сегодняшний день приложение находится в разработке
+				<Text style={{ ...styles.paragraph, color: colors.text }}>
+					{"\t\t\t\t"}Цель приложения - упрощение взаимодействия с клиентами при
+					продаже своих услуг.
 				</Text>
+				<Text style={{ ...styles.paragraph, color: colors.text }}>
+					{"\t\t\t\t"}На сегодняшний день приложение находится в разработке
+				</Text>
+
+				<Text style={{ ...styles.paragraph, color: colors.text }}>
+					{"\t\t\t\t"}Если у Вас появились предложения или замечания по
+					приложению, то сообщите об этом разработчику напрямую:
+				</Text>
+				<View style={styles.contacts}>
+					<ContactIcon
+						iconName="vk"
+						backgroundColor="#597da3"
+						url="https://vk.com/escalion"
+					/>
+					<ContactIcon
+						iconName="whatsapp"
+						backgroundColor="#43d854"
+						url="whatsapp://send?phone=79138370020"
+					/>
+					<ContactIcon
+						iconName="telegram"
+						backgroundColor="#0088cc"
+						url="http://t.me/escalion"
+					/>
+				</View>
+				<View style={{ ...styles.developer, borderColor: colors.card }}>
+					<View style={{ flex: 1 }}>
+						<Text style={{ ...styles.paragraph, color: colors.text }}>
+							Разработчик:
+						</Text>
+						<Text
+							style={{
+								...styles.paragraph,
+								fontStyle: "italic",
+								color: colors.text,
+							}}
+						>
+							Алексей Белинский
+						</Text>
+						<TouchableOpacity
+							onPress={() => Linking.openURL("https://escalion.ru")}
+						>
+							<Text
+								style={{
+									...styles.paragraph,
+									fontStyle: "italic",
+									color: colors.accent,
+								}}
+							>
+								Escalion.ru
+							</Text>
+						</TouchableOpacity>
+					</View>
+
+					<Image
+						style={{
+							width: 96,
+							height: 96,
+						}}
+						source={require("../../assets/logo-dev.png")}
+						// resizeMethod="scale"
+						resizeMode="cover"
+					/>
+				</View>
 			</View>
+
 			<View style={styles.bottom}>
 				<Text style={{ ...styles.version, color: colors.text }}>
 					Версия: 0.0.1
@@ -54,6 +146,23 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 5,
 	},
+	developer: {
+		borderTopWidth: 2,
+		borderBottomWidth: 2,
+		flexDirection: "row",
+		alignItems: "center",
+		marginVertical: 30,
+		paddingHorizontal: 20,
+	},
+	contacts: {
+		flexDirection: "row",
+		justifyContent: "space-evenly",
+		marginTop: 20,
+	},
+	paragraph: {
+		fontSize: 18,
+		marginBottom: 6,
+	},
 	content: {
 		flex: 1,
 	},
@@ -63,5 +172,10 @@ const styles = StyleSheet.create({
 	},
 	version: {
 		fontSize: 14,
+	},
+	contact: {
+		alignItems: "center",
+		justifyContent: "center",
+		borderRadius: 200,
 	},
 })
