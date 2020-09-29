1d1cdea67fa5628f2325f126cb32905916238ada
Aleksei Belinskiy <escalion86@gmail.com>
2020-09-30 01:04:06 +0700

    Fixed Fab theme color

--- src/components/Fab.js
+++ src/components/Fab.js
@@ -2,22 +2,27 @@ import React from "react"
 import { FAB } from "react-native-paper"
 import { StyleSheet } from "react-native"
 
+import { useTheme } from "@react-navigation/native"
+
 const Fab = ({
 	visible = true,
 	onPress = () => {},
 	style = {},
 	label = "",
-}) => (
-	<FAB
-		style={{ ...styles.fab, ...style }}
-		icon="plus"
-		animated={true}
-		// color={colors.accent}
-		visible={visible}
-		onPress={onPress}
-		label={label}
-	/>
-)
+}) => {
+	const { colors } = useTheme()
+	return (
+		<FAB
+			style={{ ...styles.fab, ...style }}
+			icon="plus"
+			animated={true}
+			color={colors.accentText}
+			visible={visible}
+			onPress={onPress}
+			label={label}
+		/>
+	)
+}
 
 export default Fab
 
