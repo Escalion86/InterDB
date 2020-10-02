7904f86d81e66fe628d0f894e30815cbdcba94a2
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-03 01:43:51 +0700

    Update wordForm helper

--- src/helpers/wordForm.js
+++ src/helpers/wordForm.js
@@ -1,8 +1,10 @@
-const wordForm = function (num, wordsArr) {
+const wordForm = function (num, wordsArr, showNumber = true) {
 	const cases = [2, 0, 1, 1, 1, 2]
-	return wordsArr[
-		num % 100 > 4 && num % 100 < 20 ? 2 : cases[num % 10 < 5 ? num % 10 : 5]
-	]
+	return `${showNumber ? num + " " : ""}${
+		wordsArr[
+			num % 100 > 4 && num % 100 < 20 ? 2 : cases[num % 10 < 5 ? num % 10 : 5]
+		]
+	}`
 }
 
 export default wordForm
