798bed70097905224f6bda27e46c20cbfd198520
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-05 16:07:06 +0700

    Added timing columns to events DB

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 import * as SQLite from "expo-sqlite"
 import dbTemplate, { prepareForDB } from "./dbTemplate"
 
-const DBName = "events16.db"
+const DBName = "events17.db"
 
 let db = SQLite.openDatabase(DBName)
 
