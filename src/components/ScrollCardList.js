455ec081b2772e716a746de986ba235f23c82d82
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-07 02:21:01 +0700

    Added ScrollCardList component

---
+++ src/components/ScrollCardList.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { View, ScrollView, StyleSheet } from "react-native"
+
+const ScrollCardList = ({ data, renderItem, containerStyle = {} }) => {
+	const items = data.map((item) => renderItem(item))
+
+	return (
+		<View style={containerStyle}>
+			<ScrollView style={styles.list}>{items}</ScrollView>
+		</View>
+	)
+}
+
+export default ScrollCardList
+
+const styles = StyleSheet.create({
+	list: {
+		width: "100%",
+		padding: 0,
+		margin: 0,
+		height: "100%",
+	},
+})
