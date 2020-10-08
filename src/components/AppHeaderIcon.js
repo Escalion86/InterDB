3c6d61a9a5a3446059d6b892c0c4ac1133ab8435
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-08 23:44:11 +0700

    Fixed iconSize in HeaderIcons

--- src/components/AppHeaderIcon.js
+++ src/components/AppHeaderIcon.js
@@ -1,17 +1,16 @@
 import React from "react"
-import { Platform } from "react-native"
 import { HeaderButton } from "react-navigation-header-buttons"
 import { Ionicons } from "@expo/vector-icons"
 import { useTheme } from "@react-navigation/native"
 
 export const AppHeaderIcon = (props) => {
-  const { colors } = useTheme()
-  return (
-    <HeaderButton
-      {...props}
-      iconSize={24}
-      IconComponent={Ionicons}
-      color={Platform.OS === "android" ? colors.text : colors.background}
-    />
-  )
+	const { colors, iconSize } = useTheme()
+	return (
+		<HeaderButton
+			{...props}
+			iconSize={iconSize.small}
+			IconComponent={Ionicons}
+			color={colors.icon}
+		/>
+	)
 }
