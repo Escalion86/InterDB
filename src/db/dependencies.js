7a9a88e10981e1006317be85503397045402f65d
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-27 21:38:09 +0700

    Removed "встреча" & "передано" in status event

--- src/db/dependencies.js
+++ src/db/dependencies.js
@@ -1,9 +1,9 @@
 export const statusIconDependencies = {
   Заметка: { name: 'md-create', color: '#555555' },
   'Есть вопросы': { name: 'ios-help-circle', color: '#880088' },
-  'Назначена встреча': { name: 'md-cafe', color: '#cc5511' },
+  // 'Назначена встреча': { name: 'md-cafe', color: '#cc5511' },
   Принято: { name: 'md-add-circle', color: '#000088' },
-  Передано: { name: 'ios-person', color: '#880088' },
+  // Передано: { name: 'ios-person', color: '#880088' },
   Отменено: { name: 'ios-remove-circle', color: '#bb0000' },
   Выполнено: { name: 'ios-checkmark-circle', color: '#006600' },
 }
