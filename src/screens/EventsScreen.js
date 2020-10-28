c82cf98816089cb45eeac69bf6c187de28f63a8b
Aleksei Belinskiy <escalion86@gmail.com>
2020-10-29 01:34:14 +0700

    Added month filter & badges in

--- src/screens/EventsScreen.js
+++ src/screens/EventsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
   StyleSheet,
@@ -7,6 +7,8 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   ToastAndroid,
+  ScrollView,
+  Dimensions,
 } from 'react-native'
 import {
   Menu,
@@ -31,6 +33,8 @@ import {
   ModalFinanceIncome,
   ModalFinanceOutcome,
 } from '../components/Modals/modalsFinance'
+import { DevDropDownPicker } from '../components/devComponents'
+import { Badge } from 'react-native-paper'
 
 // const ModalFinanceIncome = ({
 //   onAddFinance,
@@ -123,6 +127,82 @@ import {
 //   )
 // }
 
+const windowWidth = Dimensions.get('screen').width
+const monthItemWidth = 64
+
+const MonthItem = ({
+  text = '',
+  active = false,
+  onPress = () => {},
+  badge = 0,
+}) => {
+  const { colors } = useTheme()
+  return (
+    <TouchableOpacity
+      style={{
+        width: monthItemWidth,
+        height: 40,
+        borderColor: colors.border,
+        borderBottomColor: active ? colors.accent : null,
+        borderWidth: 1,
+        backgroundColor: colors.card,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        // marginHorizontal: 1,
+      }}
+      activeOpacity={0.5}
+      underlayColor="#DDDDDD"
+      onPress={onPress}
+    >
+      <Text
+        style={{
+          color: colors.text,
+          fontSize: fontSize.medium,
+          marginRight: badge > 0 ? 10 : 0,
+        }}
+      >
+        {text}
+      </Text>
+      {badge > 0 ? (
+        <Badge size={18} style={{ position: 'absolute', top: 3, right: 3 }}>
+          {badge}
+        </Badge>
+      ) : null}
+    </TouchableOpacity>
+  )
+}
+
+const months = [
+  'янв',
+  'фев',
+  'мар',
+  'апр',
+  'май',
+  'июн',
+  'июл',
+  'авг',
+  'сен',
+  'окт',
+  'ноя',
+  'дек',
+]
+
+const setMonthItems = (filter, setFilter = () => {}, eventsInMonths) =>
+  months.map((month, index) => (
+    <MonthItem
+      key={index}
+      text={month}
+      active={filter.month === index}
+      onPress={() => {
+        if (filter.month !== index) {
+          setFilter({ month: index, year: filter.year })
+        }
+      }}
+      badge={eventsInMonths[index]}
+    />
+  ))
+
 const EventsScreen = ({ navigation, route }) => {
   const theme = useTheme()
   const { colors } = theme
@@ -130,16 +210,40 @@ const EventsScreen = ({ navigation, route }) => {
   const { Popover } = renderers
   const [sorting, setSorting] = useState('dateDESC')
   const [modal, setModal] = useState(null)
+  const [filter, setFilter] = useState({ month: 0, year: 2020, pos: 0 })
+  // const [filterScrollPos, setFilterScrollPos] = useState(0)
+
+  const scrollViewRef = useRef()
+
   // const [income, setIncome] = useState(0)
   // const [modalFinanceEventIncome, setModalFinanceEventIncome] = useState(false)
 
-  const events = useSelector((state) => state.event.events)
+  let events = useSelector((state) => state.event.events)
   const services = useSelector((state) => state.service.services)
   const clients = useSelector((state) => state.client.clients)
   // const finances = useSelector((state) => state.finance.finances)
   const loading = useSelector((state) => state.event.loading)
 
   const { dev } = useContext(AppContext)
+
+  let years = [new Date().getFullYear()]
+  const eventsInMonths = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  events = events.filter((event) => {
+    const year = new Date(event.date).getFullYear()
+    const month = new Date(event.date).getMonth()
+    if (!years.includes(year)) {
+      years.push(year)
+    }
+    eventsInMonths[month] += 1
+    return month === filter.month
+  })
+
+  years = years
+    .sort((a, b) => (a.date > b.date ? 1 : -1))
+    .map((year) => {
+      return { name: year + '', value: year + '' }
+    })
 
   // const showModalFinanceEventIncome = (event, incomeLeft) => {
   //   setIncome(incomeLeft >= 0 ? incomeLeft : 0)
@@ -312,6 +416,18 @@ const EventsScreen = ({ navigation, route }) => {
     })
   }, [events, theme, services, clients, sorting, dev])
 
+  useEffect(() => {
+    if (scrollViewRef.current) {
+      scrollViewRef.current.scrollTo({
+        x:
+          filter.month * monthItemWidth -
+          (windowWidth - 92) / 2 +
+          monthItemWidth / 2,
+        animated: true,
+      })
+    }
+  })
+
   console.log('render EventsScreen Header finished')
 
   if (loading) {
@@ -325,21 +441,98 @@ const EventsScreen = ({ navigation, route }) => {
 
   console.log('render EventsScreen loading skipped')
 
+  const monthItems = useMemo(
+    () => setMonthItems(filter, setFilter, eventsInMonths),
+    [filter.month, filter.year, eventsInMonths]
+  )
+
+  const Filter = () => {
+    return (
+      <View
+        style={{
+          width: '100%',
+          height: 40,
+          marginBottom: 2,
+          flexDirection: 'row',
+        }}
+      >
+        {/* <ScrollableTabView
+          style={{ marginTop: 20 }}
+          initialPage={2}
+          renderTabBar={() => <ScrollableTabBar />}
+        >
+          <Text tabLabel="Tab #1">My</Text>
+          <Text tabLabel="Tab #2 word word">favorite</Text>
+          <Text tabLabel="Tab #3 word word word">project</Text>
+          <Text tabLabel="Tab #4 word word word word">favorite</Text>
+          <Text tabLabel="Tab #5">project</Text>
+        </ScrollableTabView> */}
+
+        <ScrollView
+          style={{ flex: 1, maxHeight: 40 }}
+          horizontal={true}
+          ref={scrollViewRef}
+          showsHorizontalScrollIndicator={false}
+          // onContentSizeChange={() =>
+          //   scrollViewRef.current.scrollToEnd({ animated: true })
+          // }
+          // onScrollEndDrag={(event) => {
+          //   setFilterScrollPos(event.nativeEvent.contentOffset.x)
+          // }}
+          // contentOffset={{ x: filterScrollPos, y: 0 }}
+          // ref={
+          //   (ref) => {
+          //     scrollViewRef =
+          //   } // !!
+          // }
+          scrollEventThrottle={16}
+          decelerationRate="fast"
+          // pagingEnabled
+          // snapToEnd={true}
+        >
+          {monthItems}
+        </ScrollView>
+        <DevDropDownPicker
+          tables={years}
+          tableValue="value"
+          placeholder="Выберите календарь"
+          defaultValue={filter.year + ''}
+          onChangeItem={(value) => {
+            setFilter({ month: filter.month, year: value.value })
+          }}
+          onPress={() => {}}
+          // disabled={!selectedTable}
+          style={{
+            width: 90,
+            height: '100%',
+            padding: 0,
+            marginLeft: 2,
+            marginTop: 0,
+            borderRadius: 0,
+          }}
+        />
+      </View>
+    )
+  }
+
   if (events.length === 0) {
     console.log('render EventsScreen events = 0')
     return (
-      <View style={styles.center}>
-        <Text style={{ fontSize: fontSize.giant, color: colors.text }}>
-          Событей нет
-        </Text>
-        <Fab
-          visible={true}
-          onPress={() => {
-            navigation.navigate('CreateEvent')
-          }}
-          label="Добавить событие"
-        />
-      </View>
+      <>
+        <Filter />
+        <View style={styles.center}>
+          <Text style={{ fontSize: fontSize.giant, color: colors.text }}>
+            Событей нет
+          </Text>
+          <Fab
+            visible={true}
+            onPress={() => {
+              navigation.navigate('CreateEvent')
+            }}
+            label="Добавить событие"
+          />
+        </View>
+      </>
     )
   }
 
@@ -357,69 +550,72 @@ const EventsScreen = ({ navigation, route }) => {
   console.log('render EventsScreen events = 0 skipped')
 
   return (
-    <View style={styles.container}>
-      <MainFlatListWithFab
-        data={events}
-        renderItem={({ item }) => (
-          <EventCard
-            navigation={navigation}
-            event={item}
-            onDelete={() => modalDelete(item)}
-            financeIncome={(incomeLeft) => {
-              // showModalFinanceEventIncome(item, incomeLeft)
-              setModal(
-                <ModalFinanceIncome
-                  onOuterClick={() => setModal(null)}
-                  incomeFact={item.finance_price - incomeLeft}
-                  incomePlan={item.finance_price}
-                  onAddFinance={(income, comment, date) =>
-                    dispatch(
-                      addFinance({
-                        event: item.id,
-                        type: 'income',
-                        sum: income,
-                        comment,
-                        date: date,
-                      })
-                    )
-                  }
-                />
-              )
-            }}
-            financeOutcome={(outcomeLeft) => {
-              const outcomePlan =
-                item.finance_road +
-                item.finance_organizator +
-                item.finance_assistants +
-                item.finance_consumables
-              // showModalFinanceEventIncome(item, incomeLeft)
-              setModal(
-                <ModalFinanceOutcome
-                  onOuterClick={() => setModal(null)}
-                  outcomeFact={outcomePlan - outcomeLeft}
-                  outcomePlan={outcomePlan}
-                  onAddFinance={(outcome, comment, date) =>
-                    dispatch(
-                      addFinance({
-                        event: item.id,
-                        type: 'outcome',
-                        sum: outcome,
-                        comment,
-                        date: date,
-                      })
-                    )
-                  }
-                />
-              )
-            }}
-          />
-        )}
-        onPressFab={() => {
-          navigation.navigate('CreateEvent')
-        }}
-      />
-      {modal}
-    </View>
+    <>
+      <Filter />
+      <View style={styles.container}>
+        <MainFlatListWithFab
+          data={events}
+          renderItem={({ item }) => (
+            <EventCard
+              navigation={navigation}
+              event={item}
+              onDelete={() => modalDelete(item)}
+              financeIncome={(incomeLeft) => {
+                // showModalFinanceEventIncome(item, incomeLeft)
+                setModal(
+                  <ModalFinanceIncome
+                    onOuterClick={() => setModal(null)}
+                    incomeFact={item.finance_price - incomeLeft}
+                    incomePlan={item.finance_price}
+                    onAddFinance={(income, comment, date) =>
+                      dispatch(
+                        addFinance({
+                          event: item.id,
+                          type: 'income',
+                          sum: income,
+                          comment,
+                          date: date,
+                        })
+                      )
+                    }
+                  />
+                )
+              }}
+              financeOutcome={(outcomeLeft) => {
+                const outcomePlan =
+                  item.finance_road +
+                  item.finance_organizator +
+                  item.finance_assistants +
+                  item.finance_consumables
+                // showModalFinanceEventIncome(item, incomeLeft)
+                setModal(
+                  <ModalFinanceOutcome
+                    onOuterClick={() => setModal(null)}
+                    outcomeFact={outcomePlan - outcomeLeft}
+                    outcomePlan={outcomePlan}
+                    onAddFinance={(outcome, comment, date) =>
+                      dispatch(
+                        addFinance({
+                          event: item.id,
+                          type: 'outcome',
+                          sum: outcome,
+                          comment,
+                          date: date,
+                        })
+                      )
+                    }
+                  />
+                )
+              }}
+            />
+          )}
+          onPressFab={() => {
+            navigation.navigate('CreateEvent')
+          }}
+        />
+        {modal}
+      </View>
+    </>
   )
 }
 
