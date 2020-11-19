ff38c12d5799e39d5c42478e72bdf1f8b533b916
Aleksei Belinskiy <escalion86@gmail.com>
2020-11-20 00:24:41 +0700

    Update ModalDeleteEvent

--- src/components/Modals/ModalDeleteEvent.js
+++ src/components/Modals/ModalDeleteEvent.js
@@ -1,32 +1,59 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { ModalBottomMenuYesNo } from './ModalBottomMenu'
+import { useDispatch, useSelector } from 'react-redux'
+import ModalBottomMenu from './ModalBottomMenu'
 import { deleteEvent } from '../../store/actions/event'
 import { formatDateTime } from '../../helpers/date'
+import { deleteFinancesInEvent } from '../../store/actions/finance'
+import Button from '../Button'
 
 const ModalDeleteEvent = ({
   event,
-  navigation,
   callbackToCloseModal,
   callbackAfterAccept = () => {},
 }) => {
-  // if (!callbackAfterAccept)
-  //   callbackAfterAccept = () => navigation.navigate("Events")
-
   const dispatch = useDispatch()
 
+  const finances = useSelector((state) => state.finance.finances)
+  const financesInEvent = finances.filter(
+    (finance) => finance.event === event.id
+  )
+
   const ModalDeleteConfirm = (
-    <ModalBottomMenuYesNo
+    <ModalBottomMenu
       title={`Удаление события от\n${formatDateTime(event.date, true, true)}`}
       subtitle="Вы уверены что хотите удалить событие?"
-      onAccept={() => {
-        // callbackToCloseModal()
-        dispatch(deleteEvent(event))
-        callbackAfterAccept()
-      }}
       visible={true}
-      closer={callbackToCloseModal}
-    />
+      onOuterClick={callbackToCloseModal}
+    >
+      {financesInEvent.length > 0 ? (
+        <Button
+          title="Удалить вместе со связанными транзакциями"
+          btnDecline={false}
+          onPress={() => {
+            callbackToCloseModal()
+            dispatch(deleteFinancesInEvent(event.id))
+            dispatch(deleteEvent(event))
+            callbackAfterAccept()
+          }}
+        />
+      ) : null}
+      <Button
+        title="Удалить событие"
+        btnDecline={false}
+        onPress={() => {
+          callbackToCloseModal()
+          dispatch(deleteEvent(event))
+          callbackAfterAccept()
+        }}
+      />
+      <Button
+        title="Отмена"
+        btnDecline={true}
+        onPress={() => {
+          callbackToCloseModal()
+        }}
+      />
+    </ModalBottomMenu>
   )
 
   return ModalDeleteConfirm
