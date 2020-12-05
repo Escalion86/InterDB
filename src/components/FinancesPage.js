9b3fa9dcaf1b222cc45757db61b9b9dd7d0fe790
Aleksei Belinskiy <escalion86@gmail.com>
2020-12-05 16:32:53 +0700

    Updated FinancesPage

--- src/components/FinancesPage.js
+++ src/components/FinancesPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import { Text, View, ActivityIndicator } from 'react-native'
 import MainFlatListWithFab from '../components/MainFlatListWithFab'
-import { fontSize } from '../theme'
 import areEqual from '../helpers/areEqual'
 
 const FinancesPage = ({
@@ -52,34 +51,19 @@ const FinancesPage = ({
         flex: 1,
       }}
     >
-      {finances.length === 0 ? (
-        <View style={styles.center}>
-          <Text style={{ fontSize: fontSize.giant, color: colors.text }}>
-            Транзакций нет
-          </Text>
-        </View>
-      ) : (
-        <MainFlatListWithFab
-          data={finances}
-          type="finances"
-          onScroll={onScroll}
-          navigation={navigation}
-          onScrollUp={onScrollUp}
-          onScrollDown={onScrollDown}
-          onDelete={onDelete}
-          fabVisible={false}
-        />
-      )}
+      <MainFlatListWithFab
+        data={finances}
+        type="finances"
+        textIfNoData="Транзакций нет"
+        onScroll={onScroll}
+        navigation={navigation}
+        onScrollUp={onScrollUp}
+        onScrollDown={onScrollDown}
+        onDelete={onDelete}
+        fabVisible={false}
+      />
     </View>
   )
 }
 
 export default React.memo(FinancesPage, areEqual)
-
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-})
